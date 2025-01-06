7187f5d1bfaebd45f183cc40f2cce0cdac3ee5c0
Josianne <anne.jmfreitas@gmail.com>
2025-01-05 21:36:32 -0400

    ✨ introduce new feature (update product info)

--- index.js
+++ index.js
@@ -38,13 +38,93 @@ const listProducts = async () => {
     products.forEach(product => {
       console.log(
         `ID: ${product.id}\n` +
-        `Nome: ${product.name}\n` +
-        `Categoria: ${product.category}\n` +
-        `Quantidade: ${product.quantum}\n` +
-        `Preço: R$${product.price}`
+          `Nome: ${product.name}\n` +
+          `Categoria: ${product.category}\n` +
+          `Quantidade: ${product.quantum}\n` +
+          `Preço: R$${product.price}`
       )
       console.log('------------------')
     })
+  }
+}
+
+const updateProduct = async () => {
+  const identifyingProduct = await input({
+    message: 'Informe o ID do produto que deseja atualizar:'
+  })
+
+  const filteredProduct = await products.find(
+    product => product.id == identifyingProduct
+  )
+
+  if (!filteredProduct) {
+    console.log('Produto não encontrado. Verifique o ID e tente novamente.')
+  }
+
+  console.log(
+    `ID: ${filteredProduct.id}\n` +
+    `Nome: ${filteredProduct.name}\n` +
+    `Categoria: ${filteredProduct.category}\n` +
+    `Quantidade: ${filteredProduct.quantum}\n` +
+    `Preço: R$${filteredProduct.price}`
+  )
+
+  while (true) {
+    const fieldToUpdate = await select({
+      message: 'Selecione o campo do produto que deseja atualizar?',
+      choices: [
+        {
+          name: 'Nome',
+          value: 'name'
+        },
+        {
+          name: 'Categoria',
+          value: 'category'
+        },
+        {
+          name: 'Quantidade',
+          value: 'quantum'
+        },
+        {
+          name: 'Preço',
+          value: 'price'
+        },
+        {
+          name: 'Finalizar',
+          value: 'sair'
+        }
+      ]
+    })
+
+    switch (fieldToUpdate) {
+      case 'name':
+        const newName = await input({
+          message: 'Digite o novo nome do produto:'
+        })
+        filteredProduct.name = newName
+        break
+      case 'category':
+        const newCategory = await input({
+          message: 'Digite a nova categoria do produto:'
+        })
+        filteredProduct.category = newCategory
+        break
+      case 'quantum':
+        const newQuantum = await input({
+          message: 'Informe a quantidade disponível atualmente em estoque:'
+        })
+        filteredProduct.quantum = newQuantum
+        break
+      case 'price':
+        const newPrice = await input({
+          message: 'Informe o valor unitário atualizado (Ex.: R$18,90 = 1890):'
+        })
+        filteredProduct.price = (newPrice / 100).toFixed(2)
+        break
+      case 'sair':
+        console.log('Produto atualizado com sucesso!')
+        return
+    }
   }
 }
 
@@ -88,7 +168,7 @@ const start = async () => {
         await listProducts()
         break
       case 'atualizar':
-        console.log('Atualizando produtos')
+        await updateProduct()
         break
       case 'excluir':
         console.log('Excluindo produtos')
